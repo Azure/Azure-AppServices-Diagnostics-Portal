275e19683991896d668912dacf2b9c143cd37127
XiaoxuHU <xiaoxhu@microsoft.com>
2021-10-13 14:21:53 -0700

    Add for query param

--- AngularApp/projects/app-service-diagnostics/src/app/home/components/category-nav/category-nav.component.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/home/components/category-nav/category-nav.component.ts
@@ -97,7 +97,7 @@ export class CategoryNavComponent implements OnInit {
     }
 
     getCurrentItemId() {
-        if(this._activatedRoute.snapshot.params["category"] && this._route.url.endsWith("/overview")) {
+        if(this._activatedRoute.snapshot.params["category"] && this._route.url.split("?")[0].endsWith("/overview")) {
             this.currentDetectorId = null;
             return;
         }
