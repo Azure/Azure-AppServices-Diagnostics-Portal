8caab3518d5e25b91adee78cc8aea564b7bb8616
Hawk Foreste <hawfor@microsoft.com>
2022-12-21 12:26:25 -0800

    Add application insights

--- ApplensBackend/Program.cs
+++ ApplensBackend/Program.cs
@@ -76,6 +76,7 @@ namespace AppLensV3
                 webHostBuilder.ConfigureLogging((logging) =>
                 {
                     logging.ClearProviders();
+                    logging.AddApplicationInsights();
 
                     if (config.GetValue<bool>("FileLogging:Enabled"))
                     {
