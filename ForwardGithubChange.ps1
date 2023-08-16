4dcced0ff06683a2fe10f680724a02b9f4525cea
XiaoXu Hu <xiaoxhu@microsoft.com>
2023-08-16 18:52:58 +0000

    Merged PR 8611754: Forward changes from github to onebranch
    - Forward changes from github to onebranch till https://github.com/Azure/Azure-AppServices-Diagnostics-Portal/pull/2204
    - Update ForwardGithubChange.ps1 script

--- ForwardGithubChange.ps1
+++ ForwardGithubChange.ps1
@@ -1,13 +1,20 @@
 #This script is to forward github PR to OneBranch, removed once fully migrated to OneBranch
-$dateTime = Get-Date
-$userName = git config user.name
-$branchName = $userName + "/ff-" + $dateTime.ToString("MM-dd")
-$branchName = $branchName.ToLower()
-git checkout -b $branchName
+$branchName = Read-Host "Enter the branch name you want to pull from github, default is main"
+IF ([string]::IsNullOrWhitespace($branchName)) {
+    $branchName = "main"
+}
+"Branch pulling from github is " + $branchName
+$createdBranchName = Read-Host "Enter the branch name you want to create in OneBranch, default is $branchName"
+IF ([string]::IsNullOrWhitespace($createdBranchName)) {
+    $createdBranchName = $branchName
+}
+"Branch creating in OneBranch is " + $createdBranchName
+
+git checkout -b $createdBranchName
 git remote add github https://github.com/Azure/Azure-AppServices-Diagnostics-Portal.git
 git pull
 git pull github
-git merge -X theirs github/main
-"Pushing branch " + $branchName + " to remote"
-git push -u origin $branchName
+git merge -X theirs github/$branchName
+"Pushing branch " + $createdBranchName + " to remote"
+git push -u origin $createdBranchName
 git remote remove github
