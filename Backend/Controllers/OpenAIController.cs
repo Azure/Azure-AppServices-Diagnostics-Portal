858c63f46cd1997c785b9090bcef364ee29a4342
Ajay Sharma <ajsharm@microsoft.com>
2023-08-27 17:03:59 -0700

    Auth and startup logging

--- Backend/Controllers/OpenAIController.cs
+++ Backend/Controllers/OpenAIController.cs
@@ -1,31 +1,131 @@
-﻿using CommonLibrary.Services;
+﻿using CommonLibrary.Models;
+using CommonLibrary.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System;
+using System.Net.Http;
 using System.Net;
+using System;
 using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Backend.Controllers
 {
-    [Route("api/openai")]
+    [Route("api/copilot")]
     [Produces("application/json")]
+    [Authorize(AuthenticationSchemes = "ArmAuthentication")]
     public class OpenAIController : Controller
     {
         private IOpenAIService _openAIService;
         private ILogger<OpenAIController> _logger;
         private const string OpenAICacheHeader = "x-ms-openai-cache";
-        public OpenAIController(IOpenAIService openAIService, ILogger<OpenAIController> logger)
+        private OpenAIServiceConfiguration _openAIServiceConfiguration;
+        private readonly IConfiguration _configuration;
+        private readonly DocsCopilotConfiguration _docsCopilotConfiguration;
+        public OpenAIController(IConfiguration config, IOpenAIService openAIService, ILogger<OpenAIController> logger, OpenAIServiceConfiguration openAIServiceConfiguration)
         {
+            _configuration = config;
+            _docsCopilotConfiguration = config.GetSection("DocsCopilot").Get<DocsCopilotConfiguration>();
             _logger = logger;
             _openAIService = openAIService;
+            _openAIServiceConfiguration = openAIServiceConfiguration;
         }
 
         [HttpGet("enabled")]
         public async Task<IActionResult> IsEnabled()
         {
             return Ok(_openAIService.IsEnabled());
+        }
+
+        [HttpGet("docscopilot/enabled")]
+        public async Task<IActionResult> IsDocsCopilotEnabled()
+        {
+            return Ok(_docsCopilotConfiguration.Enabled);
+        }
+
+        [HttpPost("runTextCompletion")]
+        public async Task<IActionResult> RunTextCompletion([FromBody] CompletionModel completionModel)
+        {
+            if (!_openAIService.IsEnabled())
+            {
+                return StatusCode(422, "Text Completion Feature is currently disabled.");
+            }
+
+            if (completionModel == null || completionModel.Payload == null)
+            {
+                return BadRequest("Please provide completion payload in the request body");
+            }
+
+            try
+            {
+                // Check if client has requested cache to be disabled
+                bool cachingEnabled = bool.TryParse(GetHeaderOrDefault(Request.Headers, OpenAICacheHeader, true.ToString()), out var cacheHeader) ? cacheHeader : true;
+                var chatResponse = await _openAIService.RunTextCompletion(completionModel, cachingEnabled);
+
+                return Ok(chatResponse);
+            }
+            catch (HttpRequestException reqEx)
+            {
+                _logger.LogError($"OpenAICallError: {reqEx.StatusCode} {reqEx.Message}");
+                switch (reqEx.StatusCode)
+                {
+                    case HttpStatusCode.Unauthorized:
+                    case HttpStatusCode.Forbidden:
+                    case HttpStatusCode.NotFound:
+                    case HttpStatusCode.InternalServerError:
+                        return new StatusCodeResult(500);
+                    case HttpStatusCode.BadRequest:
+                        return BadRequest("Malformed request");
+                    default:
+                        return new StatusCodeResult((int)reqEx.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return StatusCode(500, "An error occurred while processing the text completion request.");
+            }
+        }
+
+        [HttpPost("runChatCompletion")]
+        public async Task<IActionResult> RunChatCompletion([FromBody] RequestChatPayload chatPayload)
+        {
+            if (!_openAIService.IsEnabled())
+            {
+                return StatusCode(422, "Chat Completion Feature is currently disabled.");
+            }
+
+            if (chatPayload == null)
+            {
+                return BadRequest("Request body cannot be null or empty");
+            }
+
+            if (chatPayload.Messages == null || chatPayload.Messages.Length == 0)
+            {
+                return BadRequest("Please provide list of chat messages in the request body");
+            }
+
+            try
+            {
+                var chatResponse = await _openAIService.RunChatCompletion(chatPayload.Messages.ToList(), chatPayload.MetaData);
+
+                if (chatResponse != null)
+                {
+                    return Ok(chatResponse);
+                }
+                else
+                {
+                    _logger.LogError("OpenAIChatCompletionError: chatResponse is null.");
+                    return StatusCode(500, "chatResponse is null");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"OpenAIChatCompletionError: {ex}");
+                return StatusCode(500, "An error occurred while processing the chat completion request.");
+            }
         }
 
         private static string GetHeaderOrDefault(IHeaderDictionary headers, string headerName, string defaultValue = "")
--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -13,14 +13,20 @@ using Microsoft.AspNetCore.Mvc;
 using Backend.Hubs;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Backend.Services.ArmTokenValidator;
 using CommonLibrary.Models;
 using CommonLibrary.Services;
+using System.Linq;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Backend
 {
     public class Startup
     {
+        private ILogger<Startup> _logger;
         public Startup(IWebHostEnvironment env)
         {
             Environment = env;
@@ -44,7 +50,6 @@ namespace Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             var applicationInsightsOptions = new ApplicationInsightsServiceOptions
             {
                 InstrumentationKey = Configuration["ApplicationInsights:InstrumentationKey"],
@@ -78,7 +83,7 @@ namespace Backend
                 services.AddSingleton<IBingSearchService, BingSearchServiceDisabled>();
             }
 
-            if (true || !Environment.IsDevelopment())
+            if (!Environment.IsDevelopment() && Configuration.GetValue("ArmAuthentication:Enabled", false))
             {
                 services.AddAuthentication(options =>
                 {
@@ -90,38 +95,92 @@ namespace Backend
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidAudiences = new[] { Configuration["ArmAuthentication:Audience"] },
-                        IssuerValidator = ArmTokenValidator.ValidateIssuer
-                        ValidateIssuerSigningKey = false,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true
                     };
 
                     options.Events = new JwtBearerEvents
                     {
                         OnTokenValidated = context =>
                         {
+                            var allowedAppIds = Configuration["ArmAuthentication:AllowedAppIds"].ToLower().Split(',').ToList();
+                            var claimPrincipal = context.Principal;
+                            var incomingAppId = claimPrincipal.Claims.FirstOrDefault(c => c.Type.Equals("appid", StringComparison.CurrentCultureIgnoreCase)).Value.Trim();
+                            if (incomingAppId == null || !allowedAppIds.Exists(p => p.Equals(incomingAppId, StringComparison.OrdinalIgnoreCase)))
+                            {
+                                context.Fail("Unauthorized Request");
+                                _logger.LogError($"ArmAuthentication failed because incoming app id was not allowed, Incoming App Id {incomingAppId}");
+                            }
                             return Task.CompletedTask;
                         },
                         OnAuthenticationFailed = context =>
                         {
-                            //TODO: Log this with request id
+                            _logger.LogError($"ArmAuthentication failed. {context.Exception}");
                             return Task.CompletedTask;
                         },
-                        OnMessageReceived = context =>
+                        OnMessageReceived = async context =>
                         {
-
+                            string token = null;
                             context.Request.Headers.TryGetValue("Authorization", out var BearerToken);
                             if (BearerToken.Count == 0)
                             {
-                                //TODO: Log this with request id
+                                var accessToken = context.Request.Query["access_token"].ToString();
+                                if (!string.IsNullOrWhiteSpace(accessToken))
+                                {
+                                    token = accessToken;
+                                }
                             }
-                            return Task.CompletedTask;
+                            else
+                            {
+                                if (BearerToken[0].StartsWith("Bearer "))
+                                {
+                                    token = BearerToken[0].Substring("Bearer ".Length);
+                                }
+                            }
+                            if (!string.IsNullOrWhiteSpace(token)) {
+                                //Set relevant validation parameters for the tenant of the token
+                                context.Token = token;
+                                var jwtToken = new JwtSecurityToken(token);
+                                var tenantId = jwtToken.Claims.FirstOrDefault(c => c.Type == "tid")?.Value;
+
+                                if (string.IsNullOrEmpty(tenantId))
+                                {
+                                    _logger.LogError("Tenant ID claim (tid) not found in the token.");
+                                }
+
+                                var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+                                    jwtToken.Issuer.StartsWith("https://sts.windows.net/") ?
+                                    $"https://sts.windows.net/{tenantId}/.well-known/openid-configuration" :
+                                    $"https://login.microsoftonline.com/{tenantId}/v2.0/.well-known/openid-configuration",
+                                    new OpenIdConnectConfigurationRetriever());
+
+                                var openIdConfig = await configurationManager.GetConfigurationAsync(context.HttpContext.RequestAborted);
+
+                                options.TokenValidationParameters.ValidIssuer = openIdConfig.Issuer;
+                                options.TokenValidationParameters.IssuerSigningKeys = openIdConfig.SigningKeys;
+                            }
+                            else
+                            {
+                                _logger.LogInformation($"The request did not have an access token in neither the header nor the query params");
+                            }
+                            return;
                         }
                     };
                 });
             }
-
+            else
+            {
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = "ArmAuthentication";
+                    options.DefaultChallengeScheme = "ArmAuthentication";
+                }).AddScheme<AuthenticationSchemeOptions, DevelopmentAuthHandler>("ArmAuthentication", options => { });
+            }
             services.AddAuthorization();
 
             var semanticServiceConfiguration = Configuration.GetSection("SemanticService").Get<SemanticServiceConfiguration>();
+            services.AddSingleton(semanticServiceConfiguration);
 
             if (semanticServiceConfiguration != null)
             {
@@ -129,12 +188,14 @@ namespace Backend
                 services.AddSingleton<ISemanticSearchService, SemanticSearchService>();
             }
 
-            if (Configuration.GetValue("OpenAIService:Enabled", false))
+            var openAIServiceConfiguration = Configuration.GetSection("OpenAIService").Get<OpenAIServiceConfiguration>();
+
+            if (openAIServiceConfiguration.Enabled)
             {
+                services.AddSingleton(openAIServiceConfiguration);
                 if (!Environment.IsDevelopment())
                 {
-                    var connString = Configuration.GetValue<string>("OpenAIService:SignalRConnectionString");
-                    if (string.IsNullOrWhiteSpace(connString))
+                    if (string.IsNullOrWhiteSpace(openAIServiceConfiguration.SignalRConnectionString))
                     {
                         throw new Exception("OpenAI enabled but SignalR connection string not found");
                     }
@@ -145,7 +206,7 @@ namespace Backend
                         // Max message size = 2MB
                         options.MaximumReceiveMessageSize = 2 * 1024 * 1024;
                         options.MaximumParallelInvocationsPerClient = 2;
-                    }).AddAzureSignalR(connString);
+                    }).AddAzureSignalR(openAIServiceConfiguration.SignalRConnectionString);
                 }
                 else
                 {
@@ -159,9 +220,9 @@ namespace Backend
                 }
 
                 services.AddSingleton<IOpenAIService, OpenAIService>();
-                if (Configuration.GetValue("OpenAIService:RedisEnabled", false))
+                if (openAIServiceConfiguration.RedisEnabled)
                 {
-                    services.AddSingleton(async x => await RedisConnection.InitializeAsync(true, connectionString: Configuration["OpenAIService:RedisConnectionString"].ToString()));
+                    services.AddSingleton(async x => await RedisConnection.InitializeAsync(true, connectionString: openAIServiceConfiguration.RedisConnectionString));
                     services.AddSingleton<IOpenAIRedisService, OpenAIRedisService>();
                 }
                 else
@@ -196,8 +257,9 @@ namespace Backend
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            _logger = logger;
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
